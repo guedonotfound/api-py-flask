f9754159986ef710c41602b72bcf8e7f06ace1a6
Gabriel Ambrogi <g.guedes.a@hotmail.com>
2023-04-15 23:13:21 -0300

    todo list updated, create_user became 3 functions

--- main.py
+++ main.py
@@ -19,27 +19,27 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 ## LISTA USUÁRIOS##
 
 
-@app.route('/usuarios', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def get_users():
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM usuarios")
-    usuarios_bd = mycursor.fetchall()
+    mycursor.execute("SELECT * FROM users")
+    users_db = mycursor.fetchall()
     mycursor.close()
-    lista_usuarios = list()
-    for usuario in usuarios_bd:
-        lista_usuarios.append(
+    list_users = list()
+    for user in users_db:
+        list_users.append(
             {
-                'id': usuario[0],
-                'code': usuario[1],
-                'name': usuario[2],
-                'password': usuario[3],
-                'role': usuario[4]
+                'id': user[0],
+                'code': user[1],
+                'name': user[2],
+                'password': user[3],
+                'role': user[4]
             }
         )
 
     return make_response(
         jsonify(
-            info=lista_usuarios,
+            info=list_users,
             statusCode=200
         )
     )
@@ -47,24 +47,24 @@ def get_users():
 ## LISTA CARGOS##
 
 
-@app.route('/cargos', methods=['GET'])
-def get_cargos():
+@app.route('/roles', methods=['GET'])
+def get_roles():
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM cargos")
-    cargos_bd = mycursor.fetchall()
+    mycursor.execute("SELECT * FROM roles")
+    roles_db = mycursor.fetchall()
 
-    lista_cargos = list()
-    for cargo in cargos_bd:
-        lista_cargos.append(
+    list_roles = list()
+    for role in roles_db:
+        list_roles.append(
             {
-                'id': cargo[0],
-                'description': cargo[1]
+                'id': role[0],
+                'description': role[1]
             }
         )
 
     return make_response(
         jsonify(
-            info=lista_cargos,
+            info=list_roles,
             statusCode=200
         )
     )
@@ -72,66 +72,82 @@ def get_cargos():
 ## VALIDA LOGIN##
 
 
-@app.route('/usuarios/login', methods=['POST'])
-def verifica_login():
+@app.route('/users/login', methods=['POST'])
+def verify_login():
     user = request.json
     mycursor = mydb.cursor()
-    mycursor.execute(f"SELECT * FROM usuarios WHERE codigo = '{user['code']}'")
-    usuario = mycursor.fetchall()
-    if len(usuario) != 0:
-        if usuario[0][3] == user['password']:
-            user_json = {
-                "id": usuario[0][0],
-                "code": usuario[0][1],
-                "name": usuario[0][2],
-                "password": usuario[0][3],
-                "role": usuario[0][4]
-            }
+    mycursor.execute(f"SELECT * FROM users WHERE code = '{user['code']}'")
+    user_db = mycursor.fetchall()
+    if len(user_db) != 0:
+        if user_db[0][3] == user['password']:
+            if user_db[0][5] == "S":
+                user_json = {
+                    "id": user_db[0][0],
+                    "code": user_db[0][1],
+                    "name": user_db[0][2],
+                    "password": user_db[0][3],
+                    "role": user_db[0][4],
+                    "access": user_db[0][5]
+                }
 
-            return make_response(
-                jsonify(
-                    info=user_json,
-                    statusCode=200
+                return make_response(
+                    jsonify(
+                        info=user_json,
+                        statusCode=200
+                    )
                 )
-            )
+            else:
+                return make_response(
+                    jsonify(
+                        message='Acesso ainda não liberado.',
+                        statusCode=400
+                    )
+                )
         else:
             return make_response(
                 jsonify(
-                    message="Senha incorreta",
+                    message='Senha incorreta.',
                     statusCode=400
                 )
             )
     else:
         return make_response(
             jsonify(
-                message="Usuário incorreto",
+                message='Usuário incorreto.',
                 statusCode=400
             )
         )
 
-## CRIA USUÁRIO##
 
-
-@app.route('/usuarios/novo', methods=['POST'])
-def create_user():
+@app.route('/users/verify-user-code', methods=['GET'])
+def verify_user_code():
     user = request.json
-    user['name'] = user['name'].upper()
     mycursor = mydb.cursor()
-    query = "SELECT * FROM usuarios WHERE codigo = %s"
+    query = "SELECT * FROM users WHERE code = %s"
     values = (user['code'])
     mycursor.execute(query, values)
-    user_bd = mycursor.fetchall()
-    a = int(len(user['password']))
-    print(len(user['password']))
-
-    if len(user_bd) != 0:
+    user_db = mycursor.fetchall()
+    if len(user_db) == 0:
         return make_response(
             jsonify(
-                message="Já existe usuário com esse código",
+                message='Código ainda não cadastrado no banco de dados.',
+                info=user,
                 statusCode=400
             )
         )
-    elif int(len(user['password'])) > 16 or int(len(user['password'])) < 8:
+    else:
+        return make_response(
+            jsonify(
+                info=user,
+                statusCode=200
+            )
+        )
+
+
+@app.route('/users/verify-password', methods=['GET'])
+def verify_password():
+    user = request.json
+    if len(user['password']) > 32 or len(user['password']) < 8:
         return make_response(
             jsonify(
                 message='Senha precisa ter entre 8 e 16 caracteres.',
@@ -144,37 +160,58 @@ def create_user():
               re.search(r'[!@#$%^&*]', user['password'])):
         return make_response(
             jsonify(
-                message=
-                    'Senha precisa ter pelo menos um número, uma letra maiúscula, uma letra minúscula e um caracter especial.',
+                message='Senha precisa ter pelo menos um número, um caracter especial, uma letra maiúscula e uma minúscula.',
                 statusCode=400
             )
         )
     else:
-        query = "INSERT INTO usuarios (codigo, nome, senha, cargo) VALUES (%s, %s, %s, %s)"
-        values = (user['code'], user['name'], user['password'], user['role'])
-        mycursor.execute(query, values)
-        mydb.commit()
         return make_response(
             jsonify(
-                message='Usuário cadastrado com sucesso.',
                 info=user,
                 statusCode=200
             )
         )
 
+
+@app.route('/users/validade', methods=['PUT'])
+def validate_user():
+    user = request.json
+    if user['access'] == "S":
+        query = "UPDATE users SET access = 'S' WHERE code = %s"
+        values = (user['code'])
+        mycursor = mydb.cursor()
+        mycursor.execute(query, values)
+        mydb.commit()
+        return make_response(
+            jsonify(
+                message='Acesso liberado.',
+                info=user,
+                statusCode=200
+            )
+        )
+
+    else:
+        return make_response(
+            jsonify(
+                message='Acesso não liberado.',
+                statusCode=400
+            )
+        )
+
+
 ## CRIA CARGO##
 
 
-@app.route('/cargos/novo', methods=['POST'])
-def create_cargo():
-    cargo = request.json
-    cargo['description'] = cargo['description'].upper()
+@app.route('/roles/new', methods=['POST'])
+def create_role():
+    role = request.json
+    role['description'] = role['description'].upper()
     mycursor = mydb.cursor()
-    query = "SELECT * FROM cargos WHERE descricao = %s"
-    values = (cargo['description'],)
+    query = "SELECT * FROM roles WHERE description = %s"
+    values = (role['description'],)
     mycursor.execute(query, values)
-    cargo_bd = mycursor.fetchall()
-    if len(cargo_bd) != 0:
+    role_db = mycursor.fetchall()
+    if len(role_db) != 0:
         return make_response(
             jsonify(
                 message="Já existe cargo com essa descrição",
@@ -183,14 +220,14 @@ def create_cargo():
         )
 
     else:
-        query = "INSERT INTO cargos (descricao) VALUES (%s)"
-        values = (cargo['description'],)
+        query = "INSERT INTO roles (description) VALUES (%s)"
+        values = (role['description'],)
         mycursor.execute(query, values)
         mydb.commit()
         return make_response(
             jsonify(
                 message='Cargo cadastrado com sucesso.',
-                info=cargo,
+                info=role,
                 statusCode=200
             )
         )
@@ -198,33 +235,33 @@ def create_cargo():
 ## APAGA USUÁRIO##
 
 
-@app.route('/usuarios/delete', methods=['DELETE'])
+@app.route('/users/delete', methods=['DELETE'])
 def delete_user():
     user = request.json
     mycursor = mydb.cursor()
-    query = "SELECT * FROM usuarios WHERE codigo = %s"
+    query = "SELECT * FROM users WHERE code = %s"
     values = (user['code'],)
     mycursor.execute(query, values)
-    user_bd = mycursor.fetchall()
-    if len(user_bd) == 0:
+    user_db = mycursor.fetchall()
+    if len(user_db) == 0:
         return make_response(
             jsonify(
                 mensagem='Não existe usuário com esse código.',
                 statusCode=400
             )
         )
-    
+
     else:
-        query = "DELETE FROM usuarios WHERE id = %s"
-        values = (user_bd[0][0],)
+        query = "DELETE FROM users WHERE id = %s"
+        values = (user_db[0][0],)
         mycursor.execute(query, values)
         mydb.commit()
         user_json = {
-            "id": user_bd[0][0],
-            "code": user_bd[0][1],
-            "name": user_bd[0][2],
-            "password": user_bd[0][3],
-            "role": user_bd[0][4]
+            "id": user_db[0][0],
+            "code": user_db[0][1],
+            "name": user_db[0][2],
+            "password": user_db[0][3],
+            "role": user_db[0][4]
         }
         return make_response(
             jsonify(
@@ -237,41 +274,41 @@ def delete_user():
 ## APAGA CARGO##
 
 
-@app.route('/cargos/delete', methods=['DELETE'])
-def delete_cargo():
-    cargo = request.json
+@app.route('/roles/delete', methods=['DELETE'])
+def delete_role():
+    role = request.json
     mycursor = mydb.cursor()
-    query = f"SELECT * FROM cargos WHERE descricao = '{cargo['description']}'"
-    values = (cargo['description'],)
+    query = f"SELECT * FROM roles WHERE description = '{role['description']}'"
+    values = (role['description'],)
     mycursor.execute(query)
-    cargo_bd = mycursor.fetchall()
+    role_db = mycursor.fetchall()
     query = "SELECT * FROM usuarios WHERE cargo = %s"
-    values = (cargo_bd[0][0],)
+    values = (role_db[0][0],)
     mycursor.execute(query, values)
-    user_bd = mycursor.fetchall()
-    if len(cargo_bd) != 0:
-        if len(user_bd) != 0:
+    user_db = mycursor.fetchall()
+    if len(role_db) != 0:
+        if len(user_db) != 0:
             return make_response(
                 jsonify(
                     mensagem='Não é possível apagar o cargo pois possuem usuários vinculados a ele',
-                    info=cargo_bd,
+                    info=role_db,
                     statusCode=400
                 )
             )
 
         else:
             query = "DELETE FROM cargos WHERE id = %s"
-            values = (cargo_bd[0][0],)
+            values = (role_db[0][0],)
             mycursor.execute(query, values)
             mydb.commit()
-            cargo_json = {
-                "info": cargo_bd[0][0],
-                "description": cargo_bd[0][1]
+            role_json = {
+                "info": role_db[0][0],
+                "description": role_db[0][1]
             }
             return make_response(
                 jsonify(
                     mensagem='Cargo deletado com sucesso.',
-                    info=cargo_json,
+                    info=role_json,
                     statusCode=200
                 )
 
